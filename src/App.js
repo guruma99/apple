db9137c0adf6b0f5d499dc6e81eeb70622bcd776
깃허브아이디 <>
2023-03-16 16:43:41 +0900

    ✨  Reat Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from "./logo.svg";
+// import logo from './logo.svg';
 import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Cards from "./components/Main/Cards";
+import { Routes, Route, Link } from "react-router-dom";
+import Sub1 from "./components/Sub1";
+import Menu2 from "./components/Menu2";
 
 function App() {
   return (
     <div className="App">
+      {/* <Blog/> */}
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/menu1" element={<h1>menu1 페이지</h1>} />
+        <Route path="/menu2" element={<Menu2 />} />
+      </Routes>
+      {/* <Main /> */}
       <Cards />
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import logo from "../../applelogo.png";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <nav>
         <h1 className="logo">
-          <a href="./index.html">
+          <Link to="/">
             <img src={logo} alt="logo" />
-          </a>
+          </Link>
         </h1>
 
         <h2 className="visually-hidden">메뉴</h2>
         <ul className="gnb-list">
           <li className="gnb-item">
-            <a href="./menu1.html">menu1</a>
+            <Link to="/menu1">menu1</Link>
           </li>
           <li className="gnb-item">
-            <a href="./menu2.html">menu2</a>
+            <Link to="/menu2">menu2</Link>
           </li>
           <li className="gnb-item">
             <a href="./menu3.html">menu3</a>
